26aee2e36261dbd7e3644ebe824b506f81198857
iamankitawal <ankitawal95@gmail.com>
2016-03-11 18:49:15 -0800

    Create main.js

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name clientApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the clientApp
+ */
+angular.module('myapp')
+  .controller('MainCtrl', function () {
+    scope.variable = [
+      'Hello',
+      'I am Ankit',
+      'nice to meet you!!'
+    ];
+  });
