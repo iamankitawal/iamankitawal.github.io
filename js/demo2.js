56c33247ccafc5124f4237d032a741fb3b369bc9
iamankitawal <ankitawal95@gmail.com>
2016-03-13 11:25:00 -0700

    Update demo2.js

--- js/demo2.js
+++ js/demo2.js
@@ -1,7 +1,7 @@
 (function(){
 
-	var button = document.getElementById('cn-button'),
-    wrapper = document.getElementById('cn-wrapper');
+	var button = document.getElementById('cn-button2'),
+    wrapper = document.getElementById('cn-wrapper2');
 
     //open and close menu when the button is clicked
 	var open = false;
@@ -10,16 +10,16 @@
 	function handler(){
 	  if(!open){
 	    this.innerHTML = "Close";
-	    classie.add(wrapper, 'opened-nav');
+	    classie.add(wrapper, 'opened-nav2');
 	  }
 	  else{
 	    this.innerHTML = "Menu";
-		classie.remove(wrapper, 'opened-nav');
+		classie.remove(wrapper, 'opened-nav2');
 	  }
 	  open = !open;
 	}
 	function closeWrapper(){
-		classie.remove(wrapper, 'opened-nav');
+		classie.remove(wrapper, 'opened-nav2');
 	}
 
 })();
